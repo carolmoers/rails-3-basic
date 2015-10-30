0282d76b1dcd25e8e1a3dabdfb7adc2e554427e1
cmoers <carolinemoers@gmail.com>
2015-10-30 10:41:51 -0200

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  validates :email, :uniqueness => true,
+                    :lenth => { :within => 5..50 },
+                    :format => { :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i }
+  validates :password, :confirmation => true
+
   has_one :profile
   has_many :articles, :order => 'published_at DESC', :dependent => :nullify
   has_many :replies, :through => :articles, :source => :comments
