eed43dc0f609b414f68d2183d5d10f2d820cd925
cmoers <carolinemoers@gmail.com>
2015-10-30 12:14:58 -0200

    Add hashed password to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
-user = User.create :email => 'carolinemoers@gmail.com', :password => '123456'
+user = User.create :email => 'carolinemoers@gmail.com',
+                   :password => '123456',
+                   :password_confirmation => '123456' 
 
 Category.create [
   {:name => 'Programming'},
   {:name => 'Event'},
   {:name => 'Travel'}]
+
+user.articles.create :title => 'Advanced Active Record',
+                     :body => "Models need to relate to each other. In real world, ..",
+                     :published_at => Date.today
+user.articles.create :title => 'One-to-many associations',
+                    :body => "One-to-many associations describe a pattern, ..",
+                    :published_at => Date.today
