755e79b4a9ea588e87201674de824a1a282b84be
cmoers <carolinemoers@gmail.com>
2015-10-30 16:22:01 -0200

    Add comments to article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails3Basic::Application.routes.draw do
   root :to => "articles#index"
 
-  resources :articles
-    match ':controller(/:action(/:id(.:format)))'
+  resources :articles do
+    resources :comments
+  end
 
   resources :users
 end
