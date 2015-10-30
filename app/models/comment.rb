a4b8efcd5902e2eedc73f205943cfa7a2a861381
cmoers <carolinemoers@gmail.com>
2015-10-30 11:11:19 -0200

    Add validation to just comment articles that are published

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+
+  validates :name, :email, :body, :present => true
+  validates :article_should_be_published
+
+  def article_should_be_published
+    erros.add(:article_id, "is not published yet") if article && !article.published?
+  end
 end
