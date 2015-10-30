b20837d1cc5af7cfd1d7ed0a2c6180aff1e7df11
cmoers <carolinemoers@gmail.com>
2015-10-30 12:12:41 -0200

    Fix error on comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
 
-  validates :name, :email, :body, :present => true
-  validates :article_should_be_published
+  validates :name, :email, :body, :presence => true
+  validate :article_should_be_published
 
   def article_should_be_published
-    erros.add(:article_id, "is not published yet") if article && !article.published?
+    errors.add(:article_id, "is not published yet") if article && !article.published?
   end
 end
