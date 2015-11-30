42f1a53547348e38d8cdf41d86220f29583867b4
cmoers <carolinemoers@gmail.com>
2015-11-30 19:49:51 -0200

    Add login page WIP

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails3Basic::Application.routes.draw do
   end
 
   resources :users
+  resource :session
+  match '/login' => "sessions#new", :as => "login"
+  match '/logout' => "sessions#destroy", :as => "logout"
 end
